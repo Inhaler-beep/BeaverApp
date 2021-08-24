8e181a0700d1973f12819aa85ccb967e2dd3def2
Inhaler-beep <58482346+Inhaler-beep@users.noreply.github.com>
2021-08-24 22:08:08 +0530

    Successfully completed OTP

--- app/src/main/java/com/inhaler/beaverapp/VerificationActivity.java
+++ app/src/main/java/com/inhaler/beaverapp/VerificationActivity.java
@@ -3,10 +3,14 @@ package com.inhaler.beaverapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,6 +24,7 @@ public class VerificationActivity extends AppCompatActivity {
     String backendotp,phonenumber;
     private FirebaseAuth mAuth;
     private EditText code;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,7 @@ public class VerificationActivity extends AppCompatActivity {
 
         final Button verifyButton = (Button) findViewById(R.id.verify_button);
         code = (EditText) findViewById(R.id.otp_input);
+        progressBar = (ProgressBar) findViewById(R.id.verify_progressbar);
         mAuth = FirebaseAuth.getInstance();
         String otp = code.getText().toString();
 
@@ -37,6 +43,9 @@ public class VerificationActivity extends AppCompatActivity {
         verifyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                verifyButton.setVisibility(View.GONE);
+                progressBar.setVisibility(View.VISIBLE);
+
                 String otp = code.getText().toString();
                 PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.getCredential(backendotp,otp);
                 mAuth.signInWithCredential(phoneAuthCredential)
@@ -46,10 +55,16 @@ public class VerificationActivity extends AppCompatActivity {
 
                                 if(task.isSuccessful())
                                 {
+                                    verifyButton.setVisibility(View.GONE);
+                                    progressBar.setVisibility(View.GONE);
                                     Toast.makeText(getApplicationContext(), "OTP Verification Completed :", Toast.LENGTH_SHORT).show();
+                                    Intent homeIntent = new Intent(VerificationActivity.this,MainActivity.class);
+                                    startActivity(homeIntent);
                                 }
                                 else
                                 {
+                                    verifyButton.setVisibility(View.VISIBLE);
+                                    progressBar.setVisibility(View.GONE);
                                     Toast.makeText(getApplicationContext(), "Verification Failed"+task.getException().getMessage().toString(), Toast.LENGTH_SHORT).show();
                                 }
 
@@ -58,6 +73,27 @@ public class VerificationActivity extends AppCompatActivity {
             }
         });
 
+        code.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                    int length = code.length();
+                    if(length == 6)
+                    {
+                        verifyButton.callOnClick();
+                    }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
     }
 }
